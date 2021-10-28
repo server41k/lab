9834d2fd193ba7358e23e06dd64e484c8b7cdf17
Server <serverchaush@mail.ru>
2021-10-28 23:20:20 +0300

    2.4

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,52 @@
 
 <v-app>
 
-<v-navigation-drawer app v-model="drawer"></v-navigation-drawer>
+<v-navigation-drawer app v-model="drawer">
+<v-list-item>
+
+<v-list-item-content>
+
+<v-list-item-title class="title">
+
+КИПУ
+
+</v-list-item-title>
+
+<v-list-item-subtitle>
+
+Учебный проект
+
+</v-list-item-subtitle>
+
+</v-list-item-content>
+
+</v-list-item>
+
+<v-divider></v-divider>
+
+<v-list dense>
+
+<v-list-item-group color="primary">
+
+<v-list-item>
+
+<v-list-item-icon>
+
+<v-icon>mdi-cake-variant</v-icon>
+
+</v-list-item-icon>
+
+<v-list-item-content>
+
+<v-list-item-title>Link One</v-list-item-title>
+
+</v-list-item-content>
+
+</v-list-item>
+
+</v-list-item-group>
+
+</v-list></v-navigation-drawer>
 
 <v-app-bar app dark color="primary">
 
