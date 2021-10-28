42aaee52a8c0aa673aed5484f9d949c03390df91
Server <serverchaush@mail.ru>
2021-10-28 22:55:17 +0300

    2.1

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,35 @@
 <template>
 
+<v-app>
+
+<v-navigation-drawer app></v-navigation-drawer>
+
+<v-app-bar app dark color="primary">
+
+<v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+<v-spacer></v-spacer>
+
+<v-toolbar-items class="hidden-sm-and-down">
+
+<v-btn text>Link One</v-btn>
+
+<v-btn text>Link Two</v-btn>
+
+<v-btn text>Link Three</v-btn>
+
+</v-toolbar-items>
+
+</v-app-bar>
+
+<v-main>
+
+<router-view></router-view>
+
+</v-main>
+
+</v-app>
+
 </template>
 
 <script>
