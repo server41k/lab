67d74f8e1042466342411d0ecc4434a3ad16823b
server41k <chaush.s.i20@gmail.com>
2021-12-16 23:04:45 +0300

    12-12.3

--- src/main.js
+++ src/main.js
@@ -4,8 +4,7 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './router/index'
 import store from './store'
-import { initializeApp } from "firebase/app"
-import { getAnalytics } from "firebase/analytics"
+import fb from 'firebase'
 
 Vue.use(Router)
 Vue.config.productionTip = false
@@ -15,20 +14,20 @@ vuetify,
 render: h => h(App),
 router:router,
 store,
-created(){
-    const firebaseConfig = {
+  created(){
+const firebaseConfig = {
         apiKey: "AIzaSyBcIR36x_RFW-A8XK9iONwP-svtCHDtN1I",
         authDomain: "laba-22b10.firebaseapp.com",
+        databaseURL: "https://laba-22b10-default-rtdb.firebaseio.com",
         projectId: "laba-22b10",
         storageBucket: "laba-22b10.appspot.com",
         messagingSenderId: "119947626715",
         appId: "1:119947626715:web:9b1ee2c774d18fab61a4fe",
         measurementId: "G-MC6CLBDHJ8"
       };
-      
-      // Initialize Firebase
-      const app = initializeApp(firebaseConfig);
-      // eslint-disable-next-line no-unused-vars
-      const analytics = getAnalytics(app);
-    }
+  // Initialize Firebase
+  fb.initializeApp(firebaseConfig);
+  fb.analytics();
+  }
+
 }).$mount('#app')
