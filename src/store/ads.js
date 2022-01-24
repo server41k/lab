88510925837351338d43309b9701d2c6c01bfb1b
server41k <chaush.s.i20@gmail.com>
2022-01-25 00:12:28 +0300

    20

--- src/store/ads.js
+++ src/store/ads.js
@@ -159,9 +159,11 @@ export default {
 				return ad.promo
 			})
 		},
-		myAds(state) {
-			return state.ads
-        },
+    myAds(state, getters) {
+      return state.ads.filter(ad => {
+          return ad.ownerId == getters.user.id
+      })
+    },
     adById(state) {
        return id => {
        return state.ads.find(ad => ad.id == id)
