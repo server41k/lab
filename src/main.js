a0f7a605f580bcd7958c99e6b01ba40dd0985cf0
server41k <chaush.s.i20@gmail.com>
2022-01-25 00:17:53 +0300

    21

--- src/main.js
+++ src/main.js
@@ -5,10 +5,12 @@ import vuetify from './plugins/vuetify'
 import router from './router/index'
 import store from './store'
 import fb from 'firebase'
+import BuyModalComponent from '@/components/Shared/BuyModal'
 //import { initializeApp } from "firebase/app";
 //import { getAnalytics } from "firebase/analytics";
 
 Vue.use(Router)
+Vue.component('app-vue-modal',BuyModalComponent)
 Vue.config.productionTip = false
 
 new Vue({
