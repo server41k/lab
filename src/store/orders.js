48eafa6f692ab044bc779fcfae6ada7c05c42b95
server41k <chaush.s.i20@gmail.com>
2022-01-25 00:47:32 +0300

    22withtrouble

--- src/store/orders.js
+++ src/store/orders.js
@@ -1,18 +1,75 @@
+import fb from 'firebase'
+
+class Order {
+	constructor(name, phone, adId, done = false, id = null) {
+		this.name = name
+		this.phone = phone
+		this.adId = adId
+		this.done = done
+		this.id = id
+	}
+}
+
 export default {
 	state: {
         orders:[]
     },
-	mutations: {},
+	mutations: {
+        loadOrders (state, payload) {
+        state.orders = payload
+        }
+    },
 	actions: {
-        //async createOrder({commit},{name, phone, adId, ownerId}) {
-            async createOrder() {
-            await new Promise((resolve, ) => {
-                setTimeout(() => {
-                    resolve()
-                }, 4000)
-            })
-            }
+        async createOrder({commit},{name, phone, adId, ownerId}) {
+			const order = new Order(name,phone,adId)
+			commit('clearError')
+			try {
+				await fb.database().ref(`/users/${ownerId}/orders`).push(order)
+			} catch (error){
+				commit('setError', error.message)
+				throw error
+			}
+		},
+        async fetchOrders ({commit, getters}) {
+            commit('setLoaging', true)
+            commit('clearError') 
+            const resultOrders = []
+            try {
+                    const fbVal = await fb.database().ref(`/users/${getters.user.id}/orders`).once('value')
+                const orders = fbVal.val()
+                if (orders !== null) {
+                Object.keys(orders).forEach(key => {
+                const order = orders[key]
+                resultOrders.push(
+                    new Order(
+                    order.name,
+                    order.phone,
+                    order.adId,
+                    order.done,
+                    key
+                    )
+                )
+                })
+    }
+            commit('loadOrders', resultOrders)
+            commit('setLoaging', false)
+    } catch(error) {
+            commit('setError', error.message)
+            commit('setLoaging',false)
+            throw error
+        }
+    },async markOrderDone({commit,getters},payload) {
+        commit('clearError')
+        try {
+        await fb.database().ref(`/users/${getters.user.id}/orders`).child(payload).update({
+                        done: true
+                    })
+    
+        } catch (error) {
+            commit('setError', error.message)   
+            throw error
+        }
+    }
         },
-
 	getters: {}
 }
