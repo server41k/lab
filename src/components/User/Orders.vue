90747ef50a3a4fcb96c2d42f8cd070fc5a4f5aaf
server41k <chaush.s.i20@gmail.com>
2022-01-29 14:16:27 +0300

    beauty

--- src/components/User/Orders.vue
+++ src/components/User/Orders.vue
@@ -1,84 +1,77 @@
-<template> 
-	<v-container>
-		<v-layout row>
-			<v-flex xs12 sm2 offset-sm6 class="mt-5" v-if="loading">
-	<v-progress-circular 
-		:size="70"
-			:width="7"
-			color="primary"
-			indeterminate
-			></v-progress-circular>
-	</v-flex> 
-			<v-flex xs12 sm6 offset-sm3 v-else-if="!loading && orders.length !== 0">
-				<h1 class="text--secondary mb-3 mt-3">Orders</h1>
-				<v-list
-				subheader
-				two-line
-				flat
-				>
-				<v-list-item
-				v-for="order in orders"
-				:key="order.id"
-				>
-				<v-list-item-action>
-					<v-checkbox
-						:input-value="order.done"
-						color="primary"
-						@click="markDone(order)"
-					></v-checkbox>
-					</v-list-item-action>
-					<v-list-item-content>
-					<v-list-item-title>{{ order.name }}</v-list-item-title>
-					<v-list-item-subtitle>{{ order.phone }}</v-list-item-subtitle>
-					</v-list-item-content>
-					<v-list-item-action>
-						<v-btn 
-							class="primary"
-							:to="'/ad/' + order.adId"
-						>Open</v-btn>
-					</v-list-item-action>
-				</v-list-item>
-			</v-list>
-			</v-flex>
-			<v-flex xs12 sm6 offset-sm3 v-else>
-	<h1 class="text--secondary">You hane no orders</h1>
-	</v-flex>
-		</v-layout> 
-	</v-container>
+<template>
+  <v-container>
+    <v-layout row>
+      <v-flex xs12 sm2 offset-sm6 class="mt-5" v-if="loading">
+        <v-progress-circular
+          :size="70"
+          :width="7"
+          color="primary"
+          indeterminate
+        ></v-progress-circular>
+      </v-flex>
+      <v-flex xs12 sm6 offset-sm3 v-else-if="!loading && orders.length !== 0">
+        <h1 class="text--secondary mb-3 mt-3">Orders</h1>
+        <v-list subheader two-line flat>
+          <v-list-item v-for="order in orders" :key="order.id">
+            <v-list-item-action>
+              <v-checkbox
+                :input-value="order.done"
+                color="primary"
+                @click="markDone(order)"
+              ></v-checkbox>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>{{ order.name }}</v-list-item-title>
+              <v-list-item-subtitle>{{ order.phone }}</v-list-item-subtitle>
+            </v-list-item-content>
+            <v-list-item-action>
+              <v-btn class="primary" :to="'/ad/' + order.adId">Open</v-btn>
+            </v-list-item-action>
+          </v-list-item>
+        </v-list>
+      </v-flex>
+      <v-flex xs12 sm6 offset-sm3 v-else>
+        <h1 class="text--secondary">You hane no orders</h1>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 
 <script>
 export default {
-	computed: {
-		loading () {
-			return this.$store.getters.loading
-		},
-		orders () {
-			return this.$store.getters.order
-		}
-	},
-	methods: {
-		markDone(order) {
-	this.$store.dispatch('markOrderDone', order.id)
-	.then(() => {
-		order.done = true
-	})
-	.catch(() => {})
-}
-	},getters: {
-	doneOrders (state) {
-		return state.orders.filter(order => order.done)
-		},
-	undoneOrders(state) {
-		return state.orders.filter(order => !order.done)
-		},
-	orders (state, getters) {	return getters.undoneOrders.concat(getters.doneOrders)
-		}
-},
-created () {
-		this.$store.dispatch('fetchOrders')
-	}
-} 
+  computed: {
+    loading() {
+      return this.$store.getters.loading;
+    },
+    orders() {
+      return this.$store.getters.order;
+    },
+  },
+  methods: {
+    markDone(order) {
+      this.$store
+        .dispatch("markOrderDone", order.id)
+        .then(() => {
+          order.done = true;
+        })
+        .catch(() => {});
+    },
+  },
+  getters: {
+    doneOrders(state) {
+      return state.orders.filter((order) => order.done);
+    },
+    undoneOrders(state) {
+      return state.orders.filter((order) => !order.done);
+    },
+    orders(state, getters) {
+      return getters.undoneOrders.concat(getters.doneOrders);
+    },
+  },
+  created() {
+    this.$store.dispatch("fetchOrders");
+  },
+};
 </script>
 
--- src/store/ads.js
+++ src/store/ads.js
@@ -1,19 +1,19 @@
 import fb from 'firebase'
 
 class Ad {
-    constructor (title, desc, ownerId, src = '', promo = false, id = null) {
-      this.title = title
-      this.desc = desc
-      this.ownerId = ownerId
-      this.src = src
-      this.promo = promo
-      this.id = id
-    }
-  }  
+  constructor(title, desc, ownerId, src = '', promo = false, id = null) {
+    this.title = title
+    this.desc = desc
+    this.ownerId = ownerId
+    this.src = src
+    this.promo = promo
+    this.id = id
+  }
+}
 
 export default {
-	state: {
-		ads:[/*
+  state: {
+    ads: [/*
                 {
                     title:"First",
                     desc:"First Desc",
@@ -43,133 +43,132 @@ export default {
                     id:"4"
                 }
             */]
+  },
+  mutations: {
+    createAd(state, payload) {
+      state.ads.push(payload)
     },
-    mutations: {
-        createAd(state, payload){
-            state.ads.push(payload)
-        },
-        loadAds (state, payload) {
-            state.ads = payload
-        },
-        updateAd (state, {title, desc, id}) {
-          const ad = state.ads.find(a => {
-            return a.id === id
+    loadAds(state, payload) {
+      state.ads = payload
+    },
+    updateAd(state, { title, desc, id }) {
+      const ad = state.ads.find(a => {
+        return a.id === id
+      })
+      ad.title = title
+      ad.desc = desc
+    }
+  },
+  actions: {
+    createAd({ commit }, payload) {
+      payload.id = Math.random()
+      commit('createAd', payload)
+    },
+    async createAds({ commit, getters }, payload) {
+      commit('clearError')
+      commit('setLoading', true)
+      const image = payload.image
+
+      try {
+        const newAd = new Ad(
+          payload.title,
+          payload.desc,
+          getters.user.id,
+          "",
+          payload.promo,
+          payload.id
+        )
+        const fbValue = await fb.database().ref('ads').push(newAd)
+        const imageExt = image.name.slice(image.name.lastIndexOf("."))
+
+        console.log("bbbb")
+        console.log(fbValue.key)
+
+        await fb.storage().ref().child(`ads/${fbValue.key}.${imageExt}`).put(image).then(snapshot => {
+          snapshot.ref.getDownloadURL().then((downloadURL) => {
+            const src = downloadURL
+            fb.database().ref("ads").child(fbValue.key).update({ src })
+            commit('setLoading', false)
+            commit('createAd', {
+              ...newAd,
+              id: fbValue.key,
+              src
+            })
           })
-          ad.title = title
-          ad.desc = desc
-        }    
+        })
+      } catch (error) {
+        commit('setError', error.message)
+        commit('setLoading', false)
+        throw error
+      }
     },
-    actions: {
-        createAd({commit},payload){
-            payload.id = Math.random()
-            commit('createAd', payload)
-        },
-        async createAds ({commit, getters}, payload) {
-            commit('clearError')
-            commit('setLoading', true)
-            const image = payload.image
-      
-            try {
-              const newAd = new Ad(
-                payload.title,
-                payload.desc,
-                getters.user.id,
-                "",
-                payload.promo,
-                payload.id
+    async fetchAds({ commit }) {
+      commit('clearError')
+      commit('setLoading', true)
+      try {
+        //Здесь запрос к базе данных
+        const fbVal = await fb.database().ref('ads').once('value')
+        const ads = fbVal.val()
+        console.log(ads)
+        //val()
+        const resultAds = []
+        Object.keys(ads).forEach(key => {
+          const ad = ads[key]
+          resultAds.push(
+            new Ad(
+              ad.title,
+              ad.desc,
+              ad.ownerId,
+              ad.src,
+              ad.promo,
+              key
             )
-              const fbValue = await fb.database().ref('ads').push(newAd)
-              const imageExt = image.name.slice(image.name.lastIndexOf("."))
-              
-              console.log ("bbbb")
-              console.log (fbValue.key)
+          )
+        })
+        commit('loadAds', resultAds)
 
-              await fb.storage().ref().child(`ads/${fbValue.key}.${imageExt}`).put(image).then(snapshot => {
-                snapshot.ref.getDownloadURL().then((downloadURL) => {
-                  const src = downloadURL
-                  fb.database().ref("ads").child(fbValue.key).update({ src })
-      
-              commit('setLoading', false)
-              commit('createAd', {
-                ...newAd,
-                id: fbValue.key,
-                src
-                })
-              })
-              })
-            } catch (error) {
-              commit('setError', error.message)
-              commit('setLoading', false)
-              throw error
-            }
-        },
-        async fetchAds({commit}) {
-            commit('clearError')
-            commit('setLoading', true)
-            try {
-                //Здесь запрос к базе данных
-                const fbVal = await fb.database().ref('ads').once('value')
-                const ads = fbVal.val()
-                console.log(ads)
-                //val()
-                const resultAds = []
-                Object.keys(ads).forEach(key => {
-                    const ad = ads[key]
-                    resultAds.push(
-                      new Ad(
-                        ad.title,
-                        ad.desc,
-                        ad.ownerId,
-                        ad.src,
-                        ad.promo,
-                        key
-                      )
-                    )
-                  })
-                  commit('loadAds', resultAds)
 
-                  
-                commit('setLoading', false)
-            }  catch (error) {
-                commit('setError', error.message)
-                commit('setLoading', false)
-                throw error
-            }
-        },
-        async updateAd ({commit},{title,desc,id}) {
-          commit('clearError')
-            commit('setLoading', true)
-            try {
-              await fb.database().ref("ads").child(id).update({ title, desc })
-              commit('updateAd',{ title, desc, id})
-              commit('setLoading', false)
-            } catch (error) {
-              commit('setError', error.message)
-              commit('setLoading', false)
-              throw error
-            }
-        }
-    },   
-	getters: {
-		ads(state) {
-			return state.ads
-		},
-		promoAds(state) {
-			return state.ads.filter(ad => {
-				return ad.promo
-			})
-		},
+        commit('setLoading', false)
+      } catch (error) {
+        commit('setError', error.message)
+        commit('setLoading', false)
+        throw error
+      }
+    },
+    async updateAd({ commit }, { title, desc, id }) {
+      commit('clearError')
+      commit('setLoading', true)
+      try {
+        await fb.database().ref("ads").child(id).update({ title, desc })
+        commit('updateAd', { title, desc, id })
+        commit('setLoading', false)
+      } catch (error) {
+        commit('setError', error.message)
+        commit('setLoading', false)
+        throw error
+      }
+    }
+  },
+  getters: {
+    ads(state) {
+      return state.ads
+    },
+    promoAds(state) {
+      return state.ads.filter(ad => {
+        return ad.promo
+      })
+    },
     myAds(state, getters) {
       return state.ads.filter(ad => {
-          return ad.ownerId == getters.user.id
+        return ad.ownerId == getters.user.id
       })
     },
     adById(state) {
-       return id => {
-       return state.ads.find(ad => ad.id == id)
-       }
+      return id => {
+        return state.ads.find(ad => ad.id == id)
+      }
     },
 
-            
+
   }
 }
